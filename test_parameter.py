c8d3830d26d61d9613d3dd9170312147049c322a
xiancaigua <2986034557@qq.com>
2025-04-07 15:42:10 +0800

    amadues

---
+++ test_parameter.py
@@ -0,0 +1,17 @@
+TEST_N_AGENTS = 4  # the number of agents in the test 如果数量大于4不要可视化！
+
+NODE_INPUT_DIM = 5
+OTHER_INFO_INPUT_DIM = 3
+EMBEDDING_DIM = 64
+K_SIZE = 25  # the number of neighbors
+
+USE_GPU = True  # do you want to use GPUS?
+NUM_GPU = 1
+NUM_META_AGENT = 4  # the number of processes
+FOLDER_NAME = 'clean'
+model_path = f'model/{FOLDER_NAME}'
+gifs_path = f'results/{FOLDER_NAME}/gifs'
+
+NUM_TEST = 100
+NUM_RUN = 1
+SAVE_GIFS = True  # do you want to save GIFs
